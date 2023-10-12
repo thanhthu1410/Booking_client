dc0aaf05262bef2f27f69cc0764b3a01826ef33d
Tai272620 <tai272620967@gmail.com>
2023-10-12 17:12:53 +0700

    RouteSetup

--- src/routes/RouteSetup.tsx
+++ src/routes/RouteSetup.tsx
@@ -2,6 +2,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from '../pages/homes/Home'
 import Layout from '../pages/homes/components/Layout'
+import Lazy from '@/utils/Lazy/Lazy'
 
 export default function RouteSetup() {
     return (
@@ -9,9 +10,8 @@ export default function RouteSetup() {
             <Routes>
                 <Route path='/' element={<Home />}>
                     <Route index element={<Layout />}></Route>
-
                 </Route>
-
+                <Route path="/booking" element={Lazy(() => import("@pages/booking/Booking"))()}></Route>
             </Routes>
         </BrowserRouter>
     )
