a729a06c5e4f51a9abf514c2b25603956678f9df
thanhthu1410 <thanhthu9c@gmail.com>
2023-10-13 18:20:56 +0700

    login page

--- src/routes/RouteSetup.tsx
+++ src/routes/RouteSetup.tsx
@@ -4,6 +4,7 @@ import Home from '../pages/homes/Home'
 import Layout from '../pages/homes/components/Layout'
 import Lazy from '@/utils/Lazy/Lazy'
 import RouteAdmin from './RouteAdmin'
+import Login from '@/pages/login/Login'
 
 export default function RouteSetup() {
     return (
@@ -13,7 +14,9 @@ export default function RouteSetup() {
                     <Route index element={<Layout />}></Route>
                     <Route path="/booking" element={Lazy(() => import("@pages/booking/Booking"))()}></Route>
                     <Route path="/service" element={Lazy(() => import("@pages/services/Service"))()}></Route>
+                  
                 </Route>
+                <Route path="/login" element={<Login/>}></Route>
                 {RouteAdmin}
             </Routes>
         </BrowserRouter>
