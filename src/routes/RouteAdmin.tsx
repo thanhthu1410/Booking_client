244043ebc25382bf5a6fd3bfab8d430f3dd43dac
thanhthu1410 <thanhthu9c@gmail.com>
2023-10-16 16:26:07 +0700

    update admin route

--- src/routes/RouteAdmin.tsx
+++ src/routes/RouteAdmin.tsx
@@ -2,13 +2,21 @@ import Sidebar from "@/pages/admin/Admin";
 import ListCustomer from "@/pages/admin/customerManager/ListCustomer";
 import AddService from "@/pages/admin/serviceManager/AddService";
 import ListService from "@/pages/admin/serviceManager/ListService";
-import Lazy from "@/utils/Lazy/Lazy";
-import { Route } from "react-router-dom";
 
+import Voucher from "@/pages/admin/voucherManager/Voucher";
+import Lazy from "@/utils/Lazy/Lazy";
+
+import AddStaff from "@/pages/admin/staffManager/AddStaff";
+import ListStaff from "@/pages/admin/staffManager/ListStaff";
+import { Route } from "react-router-dom";
 export default
     <Route>
         <Route path='admin' element={<Sidebar />}>
             <Route index path="service" element={<ListService />}></Route>
             <Route path='addService' element={<AddService />}></Route>
+            <Route path='voucher' element={<Voucher />}></Route>
+            <Route path='listCustomer' element={<ListCustomer />}></Route>
+            <Route path='listStaff' element={<ListStaff />}></Route>
+            <Route path='addStaff' element={<AddStaff />}></Route>
         </Route>
     </Route>
