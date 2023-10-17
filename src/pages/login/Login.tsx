9d1a4e975959a64dd9f8f46506500dad53140c20
Tzung1112 <tracdinhtrung111297@gmail.com>
2023-10-17 13:51:29 +0700

    login Feature

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -10,21 +10,48 @@ import {
 import "./login.scss"
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import api from '@/services/api';
+import { message } from 'antd';
 function Login() {
+    const handleLogin = async (e:any)=>{
+        e.preventDefault();
+        const loginData = {
+            userName: e.target.userName.value,
+            password: e.target.password.value,
+        }
+       await api.authApi.login(loginData)
+       .then((res) => {
+            if(res.status!=201){
+
+            }else{
+                localStorage.setItem("token", res.data.accessToken)
+                message.success("Đăng Nhập thành công")
+                setTimeout(() => {
+                    window.location.href="/"
+                  }, 2000)
+            }
+           console.log("res", res.status);
+           
+       }).catch((err) => {
+            message.warning(err.response.data.message)
+       });
+    }
     return (
         <div className='login_container'>
             <h2>LOGIN ADMIN</h2>
             <MDBContainer className="p-3 d-flex flex-column w-50 login_container_chirld">
-
-                <MDBInput wrapperClass='mb-4' label='Email address' id='form1' type='email' />
-                <MDBInput wrapperClass='mb-4' label='Password' id='form2' type='password' />
+                <form action="" onSubmit={handleLogin}>
+                <MDBInput wrapperClass='mb-4' label='User Name' id='form1' type='text' name='userName'/>
+                <MDBInput wrapperClass='mb-4' label='Password' id='form2' type='password'  name='password'/>
 
                 <div className="d-flex justify-content-between mx-3 mb-4">
                     <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember me' />
                     <a href="!#">Forgot password?</a>
                 </div>
 
-                <MDBBtn className="mb-4">Sign in</MDBBtn>
+                <MDBBtn type='submit' className="mb-4">Sign in</MDBBtn>
+                </form>
+               
 
               
 
--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,5 +1,7 @@
+import authModule from "./modules/auth.module";
 import timeModule from "./modules/time.module";
 
 export default {
     timeApi: timeModule,
+    authApi: authModule,
 }
