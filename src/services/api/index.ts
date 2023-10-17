3c6796117e4b8bff8d8e35a86d45d711fda0f84c
Tzung1112 <tracdinhtrung111297@gmail.com>
2023-10-17 14:09:59 +0700

    login Feature Update

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,8 +1,10 @@
 import serviceModule from "./modules/service.module";
 import timeModule from "./modules/time.module";
 import voucherModule from "./modules/voucher.module";
+import authModule from "./modules/auth.module";
 
 export default {
+    authApi: authModule,
     timeApi: timeModule,
     serviceApi: serviceModule,
     voucherApi: voucherModule
