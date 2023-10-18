a941fb9fc673767a17857115299a5bb21d456890
thanhthu1410 <thanhthu9c@gmail.com>
2023-10-18 11:23:02 +0700

    update searchCss

--- src/services/api/modules/voucher.module.ts
+++ src/services/api/modules/voucher.module.ts
@@ -6,9 +6,15 @@ export default {
         return await axios.post(import.meta.env.VITE_APP_SERVER_HOST_API + "vouchers", data)
     },
     findMany: async function () {
-        return await axios.get(import.meta.env.VITE_APP_SERVER_HOST_API + "vouchers");
+        return await axios.get(import.meta.env.VITE_APP_SERVER_HOST_API + "vouchers/search");
     },
     update:  async function (voucher:Voucher) {
         return await axios.patch(import.meta.env.VITE_APP_SERVER_HOST_API + "vouchers/" + voucher.id, voucher);
     },
+    findAllPagination: async function (take: number, skip: number) {
+        return await axios.get(`${import.meta.env.VITE_APP_SERVER_HOST_API}vouchers?take=${take}&skip=${skip}`);
+      },
+    search: async (keysearch: string) => {
+        return await axios.get(import.meta.env.VITE_APP_SERVER_HOST_API + `vouchers/search?search=${keysearch}`)
+      },
 }
